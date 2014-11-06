7c972ad52b8f6633b74e5f6309432eaea8b29142
Christopherruud <dispater42@gmail.com>
2014-11-06 20:07:47 +0100

    Started outlaying the logic for handling pop-up blocking in the main app window. Will need a more refined solution

---
+++ quiz-taking-app/js/applogic.js
@@ -0,0 +1,26 @@
+var mainWindow, quizWindow;
+
+function openWin() {
+	quizWindow = window.open("quiz.html", "quizPanel", "width=250, height=250",
+			true);
+	mainWindow = window.open("/", "mainNavigationWindow", "width=250",
+			"height=250", true);
+	quizWindow.resizeTo(400, screen.height);
+	mainWindow.resizeTo(screen.width - 400, screen.height);
+	quizWindow.moveTo(screen.width - 400, 100);
+	mainWindow.moveTo(0, 100);
+	quizWindow.focus();
+}
+
+setTimeout(
+		function() {
+			if (!quizWindow || quizWindow.outerHeight === 0) {
+				// First Checking Condition Works For IE & Firefox
+				// Second Checking Condition Works For Chrome
+				alert("Popup Blocker is enabled! Please add this site to your exception list.");
+			} else {
+				// Popup Blocker Is Disabled
+				window.open('', '_self');
+				window.close();
+			}
+		}, 25);
