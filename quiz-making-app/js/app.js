4d35a060f83d60683dd5413cf9c715511a73bf52
Peter Havgar <Beo@1x-193.157.224.202.uio.no>
2014-11-12 16:43:51 +0100

    made quizedit accessible

--- quiz-making-app/js/app.js
+++ quiz-making-app/js/app.js
@@ -16,5 +16,6 @@ config(['$routeProvider', function($routeProvider , RestangularProvider) {
 //  $routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'MyCtrl3'});
   $routeProvider.when('/viewCourseedit', {templateUrl: 'partials/courseedit.html', controller: 'MyCtrl4'});
   $routeProvider.when('/viewModuleedit', {templateUrl: 'partials/moduleedit.html', controller: 'MyCtrl5'});
+  $routeProvider.when('/viewQuizedit', {templateUrl: 'partials/quizedit.html', controller: 'MyCtrl6'});
   $routeProvider.otherwise({redirectTo: '/viewCourseedit'});
 }]);
