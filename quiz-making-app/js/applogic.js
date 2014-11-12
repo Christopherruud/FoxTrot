a80a40f3e17ed520b0ce61407fbdfe51cf8024e0
Christopherruud <dispater42@gmail.com>
2014-11-12 23:23:14 +0100

    Angular is not playing well with jQuery. Dammit.

--- quiz-making-app/js/applogic.js
+++ quiz-making-app/js/applogic.js
@@ -4,51 +4,70 @@
  * used/called from the index.html file (making)
  */
 
-
 /**
  * 
- * @param domain the name of the area (domain) the course represents. Acts as the name of the course.
+ * @param domain
+ *            the name of the area (domain) the course represents. Acts as the
+ *            name of the course.
  */
-function Course(domain){
+function Course(domain) {
 	var domain;
 	this.domain = domain;
 	var level, id, nextCourse, descriptiveText;
-	var modules =[];
+	var modules = [];
 }
 
 /**
  * 
- * @param courseId the ID of the course this module belongs to
+ * @param courseId
+ *            the ID of the course this module belongs to
  */
 
-function Module(courseId){
+function Module(courseId) {
 	var moduleName, moduleId, parentId;
 	this.parentId = courseId;
 	var test = [];
-	
+
 }
 
 /**
  * 
- * @param moduleId the ID of the module this course belongs to
+ * @param moduleId
+ *            the ID of the module this course belongs to
  */
-function Test(moduleId){
+function Test(moduleId) {
 	var testId;
 	this.testId = moduleId;
 	var question;
 	var answer;
 }
 
-var json;
-
 function getCourseData() {
-	$.getJSON("/api/systemSettings/quizKey", populateCourseData(data));
+	// $.getJSON("/api/systemSettings/quizKey", populateCourseData(data));
+	var json = [ { "domain" : "testdomain",
+		"modules" : [ {
+			"moduleName" : "1",
+			"id" : 1
+		}, {
+			"moduleName" : "2",
+			"id" : 2
+		}, {
+			"moduleName" : "3",
+			"id" : 3
+		}, {
+			"moduleName" : "4",
+			"id" : 4
+		} ]
+	} ];
+	populateCourseData(json);
 }
 
-//variant function for populating the course-table with data from json if exists.
+// variant function for populating the course-table with data from json if
+// exists.
 function populateCourseData(json) {
-	$('#courseTable').empty(); //empties the coursedata -table before initializing
-	
+	$('#courseTable').empty(); // empties the coursedata -table before
+								// initializing
+
 	for (var s = 0; s < json.length; s++) {
 		var course = json[s];
 		course = explodeJSON(course);
@@ -69,12 +88,10 @@ function populateCourseData(json) {
 		tableString += '</tr>';
 		$('#courseTable').append(tableString);
 	}
-	
-	
-	
+
 }
 
-//lifted from the demo - used to turn json notation into js object...
+// lifted from the demo - used to turn json notation into js object...
 var objectStorage = new Object();
 
 function explodeJSON(object) {
