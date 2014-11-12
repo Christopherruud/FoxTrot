b8327aec78a7454c1af3e94f7d4ca31cfb8a2e6f
Christopherruud <dispater42@gmail.com>
2014-11-12 20:31:57 +0100

    tried to use some of the assignment3 - javascript - let's see if that does anything

--- quiz-making-app/js/applogic.js
+++ quiz-making-app/js/applogic.js
@@ -1,29 +1,86 @@
 /**
  * Initialize variables and updates database. 
  * All functions and variables are declared here. They are 
- * used/called from the intex.html file (making)
+ * used/called from the index.html file (making)
  */
 
+
+/**
+ * 
+ * @param domain the name of the area (domain) the course represents. Acts as the name of the course.
+ */
 function Course(domain){
 	var domain;
 	this.domain = domain;
-	var level, id;
+	var level, id, nextCourse, descriptiveText;
 	var modules =[];
-	var nextCourse;
 }
+
+/**
+ * 
+ * @param courseId the ID of the course this module belongs to
+ */
 
 function Module(courseId){
-	var parentId;
-	parentId = courseId;
+	var moduleName, moduleId, parentId;
+	this.parentId = courseId;
 	var test = [];
-	var moduleName;
-	var moduleId;
+	
 }
 
+/**
+ * 
+ * @param moduleId the ID of the module this course belongs to
+ */
 function Test(moduleId){
 	var testId;
-	testId = moduleId;
+	this.testId = moduleId;
 	var question;
 	var answer;
 }
 
+
+//variant function for populating the course-table with data from json if exists.
+function populateCourseData(json) {
+	$('#courseTable').empty(); //empties the coursedata -table before initializing
+	
+	for (var s = 0; s < json.length; s++) {
+		var course = json[s];
+		course = explodeJSON(course);
+		var tableString = "<tr>";
+		console.log('Course');
+		console.log(course);
+		// Name
+		tableString += "<td>" + course.domain + "</td>";
+
+		// Courses
+		tableString += "<td>";
+		for (var c = 0; c < course.modules.length; c++) {
+			var module = course.modules[c];
+			modules = explodeJSON(module);
+			tableString += module.moduleName + ' ';
+		}
+		tableString += '</td>';
+		tableString += '</tr>';
+		$('#courseTable').append(tableString);
+	}
+	
+	
+	
+}
+
+//lifted from the demo - used to turn json notation into js object...
+var objectStorage = new Object();
+
+function explodeJSON(object) {
+	if (object instanceof Object == true) {
+		objectStorage[object['@id']] = object;
+		console.log('Object is object');
+	} else {
+		console.log('Object is not object');
+		object = objectStorage[object];
+		console.log(object);
+	}
+	console.log(object);
+	return object;
+}
