c18e439bb2b44f3c0b50e12589d23af0fcff0e74
Peter Havgar <Beo@1x-193.157.224.202.uio.no>
2014-11-12 15:48:59 +0100

    made structure in applogic

--- quiz-making-app/js/applogic.js
+++ quiz-making-app/js/applogic.js
@@ -4,3 +4,26 @@
  * used/called from the intex.html file (making)
  */
 
+function Course(domain){
+	var domain;
+	this.domain = domain;
+	var level, id;
+	var modules =[];
+	var nextCourse;
+}
+
+function Module(courseId){
+	var parentId;
+	parentId = courseId;
+	var test = [];
+	var moduleName;
+	var moduleId;
+}
+
+function Test(moduleId){
+	var testId;
+	testId = moduleId;
+	var question;
+	var answer;
+}
+
