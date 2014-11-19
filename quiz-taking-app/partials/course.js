8c92f31a2a68da77fb16d9c7ea6949c81813daba
Peter Havgar <Beo@1x-193-157-243-103.uio.no>
2014-11-19 15:46:41 +0100

    klikker på knapper i js

--- quiz-taking-app/partials/course.js
+++ quiz-taking-app/partials/course.js
@@ -1,14 +1,32 @@
 
 var moduleHtml = 'partials/module.html';
-var courseNumber = 0;
-
-function populate(var tmp){
 
 
+var select = document.getElementById('RaceDropDown');
+var courseNumber = 1;
+//dette er hvor vi kan legge inn data i en array så vi kan manipulere den i for løkka under
+var courses = ["HowToDHIS", "Change The Layout"];
+var i;
+var btn;
+
+function populate(){
+	for (i = 0; i < courses.length; i++) {
+
+		btn = document.createElement("BUTTON");        // Create a <button> element
+		var t = document.createTextNode(courses[i] + " " + courseNumber);// Create a text node
+		btn.appendChild(t);                                // Append the text to <button>
+		document.body.appendChild(btn);                    // Append <button> to <body>
+
+		//document.getElementById(btn).addEventListener("click", hey);
+		courseNumber++;
+
+	}
 }
 
-function asd(){
-	var i = 'asdasdasd';
-	document.write(i);
+function hey (){
+	document.write("Hey baby");
 }
 
+function getModulesToCourse(){
+	document.write("hey baby baby");
+}
