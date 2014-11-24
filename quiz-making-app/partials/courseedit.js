5f009c0ccccd05368dc4e5eef1b563e9b79d434f
Mona Isabelle Yari <mona.yari@hotmail.com>
2014-11-24 17:31:49 +0100

    Edit on courseedit files

--- quiz-making-app/partials/courseedit.js
+++ quiz-making-app/partials/courseedit.js
@@ -1,16 +1,10 @@
-var idValue = document.getElementById("id").value;
-var nameValue = document.getElementById("domain").value;
-var levelValue = document.getElementById("level").value;
-var textValue = document.getElementById('descriptiveText').value;
-var moduleValue = document.getElementById('module').value;
-
-
+// js 1 - fungerer med html skjema 1 courseedit.html
 $(document).ready(function(){
     var id=1;
     
 	$("#add").click(function(){
 	    $("#table1").append
-        ('<tr valign="top"><td width="100px" align="center">'+(id++)+'</td><td width="100px">'
+        ('<tr valign="top"><td width="100px" align="center">'+(id++)+'</td><td            width="100px">'
            +$("#domain").val()+'</td><td width="100px" align="center">'
          +$("#level").val()+'</td><td width="100px" align="center">'
          +$("#descriptiveText").val()+'</td><td width="100px" align="center">'
@@ -22,5 +16,73 @@ $(document).ready(function(){
     });
 });
 
-
-
+/* Js 2 - denne koden fungere for en eller annen grunn ikke, men snakker med html skjema 2 courseedit.html
+ * 
+$(function () {
+    
+    var $courses = $('#courses');
+	var $id = $('#id');
+	var $domain = $('#domain');
+	var $level = $('level');
+	var $descriptiveText = $('descriptiveText');
+	var $module = $('module');
+    
+    var courseTemplate = $('#course-template').html();
+	
+	function addCourse(course){
+        $courses.append(Mustache.render(courseTemplate, order));
+    }
+		
+	 $ajax({
+		 type: 'GET',
+		 url: '/api/systemSettings/quizTestKey',
+		 success: function(courses){
+			 $.each(courses, function(i, course){
+				 addCourse(course); 
+		 });		 
+	 },
+	 error: function() {
+		 alert('error loading courses');	 
+	 }
+	 });
+	 
+    $('#add-course').on('click', function() {
+        
+        var course ={
+            id: $id.val(),
+            domain: $domain.val(),
+            level: $level.val(),
+            descriptiveText: $descriptiveText.val(),
+            module: $module.val(),
+        };
+            
+		    $ajax({
+				 type: 'POST',
+				 url: '/api/systemSettings/quizTestKey',
+				 data: course,
+				 success: function(newCourse) {
+					addCourse(newCourse); 		 
+            },
+			 error: function() {
+				 alert('error saving course');	 
+			 }
+            });
+        });
+    
+    $courses.delegate('.remove', 'click', function(){
+        
+        var $li =$(this).closest('li');
+        
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/systemSettings/quizTestKey' + $(this).attr('data-id'),
+            success: function (){
+                $li.fadeOut(300, function() {
+                $(this).remove();
+                });
+            }
+        });
+    });
+  });
+    
+	*/
