f19ea35c9c2298bbe462c70c96792f4d7ab69ce8
Peter Havgar <Beo@1x-193-157-223-178.uio.no>
2014-11-25 17:11:22 +0100

    Justert module.js og module.html

--- quiz-taking-app/partials/module.js
+++ quiz-taking-app/partials/module.js
@@ -1,13 +1,22 @@
 
+var moduleNumber = 1;
+var data = getCourseData();
 
+//vi må diskutere om dette er måten vi vil velge for å populere siden med valg (knapper)
 function populate(){
+    
 	for (i = 0; i < data.length; i++) {
 		var btn = document.createElement("BUTTON");        // Create a <button> element
-		btn.textContent=data[i].domain + " " + courseNumber;            
+		btn.textContent=data[i].modules[i].moduleName + " " + courseNumber;            
 		document.body.appendChild(btn);                    // Append <button> to <body>
 
-		btn.addEventListener("click", getModule);
-		courseNumber++;
-
+		//btn.addEventListener("click", getModule);
+		moduleNumber++;
+        
 	}
+}
+
+function getTest(){
+	//her må vi populere siden med innhold fra testen
+	
 }
