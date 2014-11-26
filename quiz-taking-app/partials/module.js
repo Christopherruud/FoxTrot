d95b987f20aeb0649b2757ed6a21ae26b29b0239
Peter Havgar <Beo@1x-193-157-196-171.uio.no>
2014-11-26 10:00:06 +0100

    what

--- quiz-taking-app/partials/module.js
+++ quiz-taking-app/partials/module.js
@@ -1,32 +1,33 @@
 getCourseData();
 var moduleNumber = 1;
-var data = getCourseData();
+
 
 //vi må diskutere om dette er måten vi vil velge for å populere siden med valg (knapper)
 function populate(){
-    
-	for (var Module in courses) {
-		var btn = document.createElement("BUTTON");        // Create a <button> element
-		btn.textContent=Module.moduleName + " " + courseNumber;            
-		document.body.appendChild(btn);                    // Append <button> to <body>
+	for(var i = 0; i<courses.length; i++){
+		for (var Module in Course.modules) {
+			var btn = document.createElement("BUTTON");        // Create a <button> element
+			btn.textContent=Module.moduleName + " " + moduleNumber;            
+			document.body.appendChild(btn);                    // Append <button> to <body>
 
-		//btn.addEventListener("click", getModule);
-		moduleNumber++;
-        
+			//btn.addEventListener("click", getModule);
+			moduleNumber++;
+
+		}
 	}
 }
 
 function getTest(){
 	//her må vi populere siden med innhold fra testen
-		window.location = "module.html";
-		//VIKTIG om vi vil ha en "one page app"
-		/*$.ajax({
+	window.location = "module.html";
+	//VIKTIG om vi vil ha en "one page app"
+	/*$.ajax({
 			  url: "test.html",
 			  context: document.body
 			}).done(function(response) {
 			  $( this ).addClass( "done" );
 			  console.log(response);
 			});
-	*/	
-	}
+	 */	
+}
 }
