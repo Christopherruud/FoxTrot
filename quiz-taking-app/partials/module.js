a387c85d81ecd87971e2754c91bf8c9c16697b96
Peter Havgar <Beo@1x-193-157-196-171.uio.no>
2014-11-26 11:53:43 +0100

    linker knapper taking

--- quiz-taking-app/partials/module.js
+++ quiz-taking-app/partials/module.js
@@ -1,25 +1,53 @@
-getCourseData();
+
 var moduleNumber = 1;
 
+var modules = [];
 
-//vi må diskutere om dette er måten vi vil velge for å populere siden med valg (knapper)
-function populate(){
-	for(var i = 0; i<courses.length; i++){
-		for (var Module in Course.modules) {
-			var btn = document.createElement("BUTTON");        // Create a <button> element
-			btn.textContent=Module.moduleName + " " + moduleNumber;            
-			document.body.appendChild(btn);                    // Append <button> to <body>
-
-			//btn.addEventListener("click", getModule);
-			moduleNumber++;
-
-		}
-	}
+function setModules(array){
+	console.log(array);
+	modules = array;
+	console.log(modules);
+	populateModule();
 }
 
+//vi må diskutere om dette er måten vi vil velge for å populere siden med valg (knapper)
+function populateModule(){
+	
+	for (var Module in modules) {
+		console.log(Module);
+
+		var moduleElement = modules[Module].moduleName;
+		var btn = document.createElement("BUTTON");        // Create a <button> element
+		btn.textContent=moduleElement + " " + moduleNumber;            
+		document.body.appendChild(btn);                    // Append <button> to <body>
+		//btn.addEventListener("click", getModule);
+		moduleNumber++;
+
+	}
+
+}
+/*
+function populate(courses){
+	console.log(courses);
+	for (var Course in courses) {
+		var courseElement = courses[Course];
+		console.log(courseElement);
+		var btn = document.createElement("BUTTON");        // Create a <button> element
+		btn.textContent=courseElement.domain + " " + courseNumber;            
+		document.body.appendChild(btn);                    // Append <button> to <body>
+
+		btn.addEventListener("click", function (){
+
+			getModule();});
+		courseNumber++;
+
+	}
+
+}
+ */
 function getTest(){
 	//her må vi populere siden med innhold fra testen
-	window.location = "module.html";
+	window.location = "view.html";
 	//VIKTIG om vi vil ha en "one page app"
 	/*$.ajax({
 			  url: "test.html",
@@ -29,5 +57,4 @@ function getTest(){
 			  console.log(response);
 			});
 	 */	
-}
 }
