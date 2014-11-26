d69464076033458b522686668660105cd79eef96
Christopherruud <dispater42@gmail.com>
2014-11-26 17:21:54 +0100

    More git messing about

--- quiz-making-app/js/quizedit.js
+++ quiz-making-app/js/quizedit.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
-    var id=1;
-    
-	$("#add").click(function(){
-	    $("#table3").append
-	    ('<tr valign="top"><td width="100px" align="center">'
-	    		+(id++) +'</td><td width="100px"> ' 
-	    		+$("#question").val()+'</td><td width="100px" align="center">
-	    		+$("#answer1").val()+'</td><td width="100px" align="center">
-	    		+$("#answer2").val()+'</td><td width="100px" align="center">
-	    		+$("#answer3").val()+'</td><td width="100px" align="center">
-	    		+$("#answer4").val()+'</td><td width="100px" align="center"><a href="javascript:void(0);" class="remCF">Remove</a></td> <td><td width="100px" align="center"><a href="quizedit.html">Create test</a></td></tr>');
-	}); 
-        
-    $("#table2").on('click','.remCF',function(){
+$(document).ready(function () {
+    var id = 1;
+
+    $("#add").click(function () {
+        $("#table3").append
+        ('<tr valign="top"><td width="100px" align="center">'
+        + (id++) + '</td><td width="100px"> '
+        + $("#question").val() + '</td><td width="100px" align="center">'
+        + $("#answer1").val() + '</td><td width="100px" align="center">'
+        + $("#answer2").val() + '</td><td width="100px" align="center">'
+        + $("#answer3").val() + '</td><td width="100px" align="center">'
+        + $("#answer4").val() + '</td><td width="100px" align="center"><a href="javascript:void(0);" class="remCF">Remove</a></td> <td><td width="100px" align="center"><a href="quizedit.html">Create test</a></td></tr>');
+    });
+
+    $("#table2").on('click', '.remCF', function () {
         $(this).parent().parent().remove();
     });
 });
