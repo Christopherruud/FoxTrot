cb7603ee95b0d1c5490cc90e9c6f0227d2cbd8f9
Christopher Neumann Ruud <chrinr@ifi.uio.no>
2014-11-26 10:22:08 +0100

    fixed structures

--- quiz-making-app/js/applogic.js
+++ quiz-making-app/js/applogic.js
@@ -5,50 +5,50 @@
  */
 
 /**
- *
+ * 
  * @param domain
  *            the name of the area (domain) the course represents. Acts as the
  *            name of the course.
  */
 
 function Course(domain) {
-    this.domain = domain;
-    var level, id, nextCourse, descriptiveText;
-    var modules = [];
+	this.domain = domain;
+	var level, id, nextCourse, descriptiveText;
+	var modules = [];
 }
 
 /**
- *
+ * 
  * @param level
  *            the level of the course this module belongs to
  */
 
 function Module(level) {
-    var moduleName, moduleId;
-    this.level = level;
-    var tests = [];
+	var moduleName, moduleId;
+	this.level = level;
+	var tests = [];
 
 }
 
 /**
- *
+ * 
  * @param moduleId
  *            the ID of the module this course belongs to
  */
 function Test(moduleId) {
-    var parent;
-    this.parent = moduleId;
-    var question;
-    var answer;
-    var alternatives = [];
+	var parent;
+	this.parent = moduleId;
+	var question;
+	var answer;
+	var alternatives = [];
 }
 
 function getCourseData() {
-    $.getJSON("/api/systemSettings/quizKey", function (data) {
+	$.getJSON("/api/systemSettings/quizKey", function(data) {
 
-        populateCourseData(data);
+		populateCourseData(data);
 
-    });
+	});
 
 }
 
@@ -57,107 +57,104 @@ function getCourseData() {
 var courses = [];
 
 function populateCourseData(json) {
-    var courseTable = $('#courseTable');
-    courseTable.empty(); // empties the coursedata -table before
-    // initializing
-    console.log("console.log(json)");
-    console.log(json);
+	var courseTable = $('#courseTable');
+	courseTable.empty(); // empties the coursedata -table before
+	// initializing
+	console.log("console.log(json)");
+	console.log(json);
 
-    for (var s = 0; s < json.length; s++) {
-        var course = json[s];
-        course = explodeJSON(course);
-        var tableString = "<tr>";
-        // Name
-        var tempCourse = new Course(course.domain);
-        tempCourse.level = course.level;
-        tempCourse.id = course.id;
-        tempCourse.nextCourse = course.nextCourse;
-        tempCourse.descriptiveText = course.descriptiveText;
-        tempCourse.modules = [];
+	for (var s = 0; s < json.length; s++) {
+		var course = json[s];
+		course = explodeJSON(course);
+		var tableString = "<tr>";
+		// Name
+		var tempCourse = new Course(course.domain);
+		tempCourse.level = course.level;
+		tempCourse.id = course.id;
+		tempCourse.nextCourse = course.nextCourse;
+		tempCourse.descriptiveText = course.descriptiveText;
+		tempCourse.modules = [];
 
-        courses.push(tempCourse);
+		courses.push(tempCourse);
 
-        tableString += "<td>" + course.domain + "</td>";
+		tableString += "<td>" + course.domain + "</td>";
 
-        // Modules
-        tableString += "<td>";
-        for (var c = 0; c < course.modules.length; c++) {
-            var module = course.modules[c];
-            module = explodeJSON(module);
+		// Modules
+		tableString += "<td>";
+		for (var c = 0; c < course.modules.length; c++) {
+			var module = course.modules[c];
+			module = explodeJSON(module);
 
-            var tempModule = new Module(course.level);
-            tempModule.moduleName = module.moduleName;
-            tempModule.moduleId = module.moduleId;
-            tempModule.tests = [];
+			var tempModule = new Module(course.level);
+			tempModule.moduleName = module.moduleName;
+			tempModule.moduleId = module.moduleId;
+			tempModule.tests = [];
 
-            courses[s].modules.push(tempModule);
+			courses[s].modules.push(tempModule);
 
-            for (var f = 0; f < module.tests.length; f++) {
-                var test = module.tests[f];
-                test = explodeJSON(test);
+			for (var f = 0; f < module.tests.length; f++) {
+				var test = module.tests[f];
+				test = explodeJSON(test);
 
-                var tempTest = new Test(module.moduleID);
+				var tempTest = new Test(module.moduleID);
 
-                tempTest.question = test.question;
-                tempTest.answer = test.answer;
-                tempTest.alternatives = test.alternatives.slice();
-                courses[s].modules[c].tests.push(tempTest);
-            }
+				tempTest.question = test.question;
+				tempTest.answer = test.answer;
+				tempTest.alternatives = test.alternatives.slice();
+				courses[s].modules[c].tests.push(tempTest);
+			}
 
-            tableString += module.moduleName + ' ';
-        }
-        tableString += '</td>';
-        tableString += '</tr>';
-        courseTable.append(tableString);
-    }
-    // jsonAlexander(courses);
-}
-// function for messing about with JSON
-function jsonAlexander(json) {
+			tableString += module.moduleName + ' ';
+		}
+		tableString += '</td>';
+		tableString += '</tr>';
+		courseTable.append(tableString);
+	}
 
 }
+
 // lifted from the demo - used to turn json notation into js object...
 var objectStorage = new Object();
 
 function explodeJSON(object) {
-    if (object instanceof Object == true) {
-        objectStorage[object['@id']] = object;
-        // console.log('Object is object');
-    } else {
-        console.log('Object is not object');
-        object = objectStorage[object];
-        console.log(object);
-    }
-    console.log(object);
-    return object;
+	if (object instanceof Object == true) {
+		objectStorage[object['@id']] = object;
+
+	} else {
+		console.log('Object is not object');
+		object = objectStorage[object];
+		console.log(object);
+	}
+	console.log(object);
+	return object;
 }
 
 function postCourseData(json) {
-    var jsonString = JSON.stringify(json);
-    $.ajax({
-        type: "POST",
-        contentType: "text/plain",
-        url: "/api/systemSettings/quizKey",
-        data: jsonString,
-        success: function (data) {
-            //lolno
-        },
-        dataType: "text"
-    });
+	var jsonString = JSON.stringify(json);
+	$.ajax({
+		type : "POST",
+		contentType : "text/plain",
+		url : "/api/systemSettings/quizKey",
+		data : jsonString,
+		success : function(data) {
+			// lolno
+		},
+		dataType : "text"
+	});
 
 }
 
 function postTestData(json) {
-    var jsonString = JSON.stringify(json);
-    $.ajax({
-        type: "POST",
-        contentType: "text/plain",
-        url: "/api/systemSettings/quizTestKey",
-        data: jsonString,
-        success: function (data) {
-            //lolno
-        },
-        dataType: "text"
-    });
+	var jsonString = JSON.stringify(json);
+	$.ajax({
+		type : "POST",
+		contentType : "text/plain",
+		url : "/api/systemSettings/quizTestKey",
+		data : jsonString,
+		success : function(data) {
+			// lolno
+		},
+		dataType : "text"
+	});
 
 }
