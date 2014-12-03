fd8389efa6f1831c2ef142f48a2d4bcf6815c186
Christopher Neumann Ruud <chrinr@ifi.uio.no>
2014-12-03 14:08:51 +0100

    messing about part #%#4234234

--- quiz-taking-app/js/course.js
+++ quiz-taking-app/js/course.js
@@ -2,52 +2,54 @@
 
 //var moduleHtml = 'partials/module.html';
 //var select = document.getElementById('RaceDropDown');
-var courseNumber = 1;
-//dette er hvor vi kan legge inn data i en array så vi kan manipulere den i for løkka under
+var courseNumber = 0;
+// dette er hvor vi kan legge inn data i en array så vi kan manipulere den i for
+// løkka under
 
-
-//setter opp knapper etter ønske
-function populate(courses, isInModule){
+// setter opp knapper etter ønske
+function populate(courses, isInModule) {
 	console.log(courses);
-	
-	for (var Course in courses) {
+
+	for ( var Course in courses) {
 		var courseElement = courses[Course];
 		console.log(courseElement);
-		var btn = document.createElement("BUTTON");        // Create a <button> element
-		btn.textContent=courseElement.domain + " " + courseNumber; 
-		//document.write(courseElement.descriptiveText);
-		document.body.appendChild(btn);                    // Append <button> to <body>
+		var btn; // Create a <button> element
+		if (isInModule == false) {
+			btn = document.createElement("BUTTON");
+			btn.textContent = courseElement.domain + " " + courseNumber;
+			document.body.appendChild(btn); // Append <button> to <body>
 
-		btn.addEventListener("click", function (){
-			console.log("CheckEvent");
-			console.log(courseElement.modules);
-			
-			getModule(courseElement.modules, courseElement.id, isInModule);});
-		courseNumber++;
+			btn.addEventListener("click", function() {
+				console.log("CheckEvent");
+				console.log(courseElement.modules);
+
+				courseNumber++;
+			});
+		}
+		// document.write(courseElement.descriptiveText);
+		getModule(courseElement.modules, courseElement.id, isInModule);
+
 	}
 
 }
-//henter ut modul html når man trykker på en knapp. Må sende
-//med ting her fra tilhørende modul til gitt kurs
-function getModule(element, courseId, isInModule){
+// henter ut modul html når man trykker på en knapp. Må sende
+// med ting her fra tilhørende modul til gitt kurs
+function getModule(element, courseId, isInModule) {
 	console.log(element);
 	var moduleArray = [];
 	var id = courseId;
 	console.log(id);
 	moduleArray = element;
 	console.log(moduleArray);
-	
+
 	setModules(moduleArray, id, isInModule);
-	
+
 }
 
-//window.location = "module.html";
-//VIKTIG om vi vil ha en "one page app"
-/*$.ajax({
-	  url: "module.html",
-	  context: document.body
-	}).done(function(response) {
-	  $( this ).addClass( "done" );
-	  console.log(response);
-	});
- */	
+// window.location = "module.html";
+// VIKTIG om vi vil ha en "one page app"
+/*
+ * $.ajax({ url: "module.html", context: document.body
+ * }).done(function(response) { $( this ).addClass( "done" );
+ * console.log(response); });
+ */
